4b1955f1bfd080eedefbd255804172b6020964da
SNeXT <snext2004@gmail.com>
2025-02-07 23:51:20 +0700

    adjust token's expire 3hrs

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -5,7 +5,7 @@ const generateToken = (user) => {
     return jwt.sign(
         { id: user._id, email: user.email },
         process.env.SECRET_KEY,
-        { expiresIn: "10h" }
+        { expiresIn: "3h" }
     );
 };
 
