767a0e1e87e2781172c083632697fdb9be53f99a
SNeXT <snext2004@gmail.com>
2025-02-10 17:54:53 +0700

    backend add patch and delete challenge.

--- backend/src/controllers/challenge.js
+++ backend/src/controllers/challenge.js
@@ -4,51 +4,90 @@ const { Challenge, Vocab } = require("../models/challenge");
 // @route   GET /api/challenge
 // @access  Public
 const getAllChallenges = async (req, res) => {
-    try{
-        const challenges = await Challenge.find();
-        res.json(challenges);
-    }catch(err){
-        console.log("ERROR!!!", err)
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+  try {
+    const challenges = await Challenge.find();
+    res.json(challenges);
+  } catch (err) {
+    console.log("ERROR!!!", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 // @desc    Fetch all vocab
 // @route   GET /api/challenge/:challengeId
 // @access  Public
 const getVocabChallenge = async (req, res) => {
-    try{
-        const { challengeId } = req.params;
-        const query = await Vocab.find({ 'challenge': `${challengeId}` });
-        res.json(query);
-    }catch(err){
-        console.log("ERROR!!!", err)
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+  try {
+    const { challengeId } = req.params;
+    const query = await Vocab.find({ challenge: `${challengeId}` });
+    res.json(query);
+  } catch (err) {
+    console.log("ERROR!!!", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
+// @desc    Upload challenge
+// @route   POST /api/challenge
+// @access  Admin
 const uploadChallenge = async (req, res) => {
-    try {
-      if (!req.file) {
-        return res.status(400).json({ message: "No file uploaded" });
-      }
-
-      const { name, content } = req.body;
-      const img = `images/${req.file.filename}`
-      console.log(req.file.filename)
-      const newChallenge = new Challenge({ name, content, img});
-      await newChallenge.save();
-
-      res.status(200).json({ 
-        message: "File uploaded successfully",
-        challenge: newChallenge
-      });
-  
-    } catch (error) {
-      console.error("Error uploading file:", error);
-      res.status(500).json({ message: "Server error", error: error.message });
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
     }
-  };
-  
 
-module.exports = { getAllChallenges, getVocabChallenge, uploadChallenge};
+    const { name, content } = req.body;
+    const img = `images/${req.file.filename}`;
+    console.log(req.file.filename);
+    const newChallenge = new Challenge({ name, content, img });
+    await newChallenge.save();
+
+    res.status(200).json({
+      message: "File uploaded successfully",
+      challenge: newChallenge,
+    });
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+// @desc    Patch challenge by id
+// @route   PATCH /api/challenge/:challengeID
+// @access  Admin
+const patchChallenge = async (req, res) => {
+  try {
+    const updatedChallenge = await Challenge.findByIdAndUpdate(
+      req.params.challengeId,
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedChallenge) {
+      return res.status(404).json({ message: "Challenge not found" });
+    }
+
+    res.json(updatedChallenge);
+  } catch (error) {
+    console.error("Error updating challenge:", error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+const deleteChallenge = async (req, res) => {
+  try {
+    const deletedChallenge = await Challenge.findByIdAndDelete(req.params.challengeId);
+
+    if (!deletedChallenge) {
+      return res.status(404).json({ message: "Challenge not found" });
+    }
+
+    res.json({ message: "Challenge deleted successfully" });
+
+  } catch (error) {
+    console.error("Error deleting challenge:", error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+module.exports = { getAllChallenges, getVocabChallenge, uploadChallenge, patchChallenge, deleteChallenge };
--- backend/src/routes/challenge.js
+++ backend/src/routes/challenge.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const { getAllChallenges, getVocabChallenge, uploadChallenge } = require("../controllers/challenge");
-const { authenticateJWT, adminAuthenticateJWT } = require('../middlewares/user')
-const { imageUpload } = require('../middlewares/upload') 
+const {
+  getAllChallenges,
+  getVocabChallenge,
+  uploadChallenge,
+  patchChallenge,
+  deleteChallenge
+} = require("../controllers/challenge");
+const { adminAuthenticateJWT } = require("../middlewares/user");
+const { imageUpload } = require("../middlewares/upload");
 
 const router = express.Router();
 
@@ -10,6 +16,15 @@ router.get("/", getAllChallenges);
 
 router.get("/:challengeId", getVocabChallenge);
 
-router.post("/upload", adminAuthenticateJWT, imageUpload.single("file"), uploadChallenge);
+router.post(
+  "/upload",
+  adminAuthenticateJWT,
+  imageUpload.single("file"),
+  uploadChallenge
+);
+
+router.patch("/:challengeId", adminAuthenticateJWT, patchChallenge);
+
+router.delete("/:challengeId", adminAuthenticateJWT, deleteChallenge);
 
 module.exports = router;
